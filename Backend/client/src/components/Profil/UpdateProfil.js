5c4f4ca26a9aae46b6bf708a0d96f0a145418c40
fouzi92git config --global user.email fouzi457@hotmail.frgit config --global user.name fouzi92 <fouzi457@hotmail.fr>
2022-04-15 17:53:37 +0200

    suite

--- Backend/client/src/components/Profil/UpdateProfil.js
+++ Backend/client/src/components/Profil/UpdateProfil.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import LeftNav from "../LeftNav";
 import { useSelector } from "react-redux";
 import UploadImg from "./UploadImg";
 
 const UpdateProfil = () => {
+  const [bio, setBio] = useState("");
+  const [updateForm, setUpdateForm] = useState(false);
   const userData = useSelector((state) => state.userReducer);
   return (
     <div className="profil-container">
@@ -15,6 +17,28 @@ const UpdateProfil = () => {
           <img src={userData.picture} alt="user-pic" />
 
           <UploadImg />
+        </div>
+        <div className="right-part">
+          <div className="bio-update">
+            <h3>Bio</h3>
+            {updateForm === false && (
+              <>
+                <p onClick={() => setUpdateForm(!updateForm)}>{userData.bio}</p>
+                <button onClick={() => setUpdateForm(!updateForm)}>
+                  Modifier bio
+                </button>
+              </>
+            )}
+            {updateForm && (
+              <>
+                <textarea
+                  type="text"
+                  defaultValue={userData.bio}
+                  onChange={(e) => setBio(e.target.value)}
+                ></textarea>
+              </>
+            )}
+          </div>
         </div>
       </div>
     </div>
